9a903513bd18f640968109f3c8208e077b9c0258
Jerry Young <jerryyoung2911@gmail.com>
2022-07-24 00:43:35 +0700

    Add user model

--- go.mod
+++ go.mod
@@ -5,6 +5,11 @@ go 1.17
 require github.com/labstack/echo/v4 v4.7.2
 
 require (
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.4 // indirect
+)
+
+require (
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/labstack/gommon v0.3.1 // indirect
 	github.com/mattn/go-colorable v0.1.11 // indirect
@@ -16,4 +21,5 @@ require (
 	golang.org/x/sys v0.0.0-20211103235746-7861aae1554b // indirect
 	golang.org/x/text v0.3.7 // indirect
 	golang.org/x/time v0.0.0-20201208040808-7e3f01d25324 // indirect
+	gorm.io/gorm v1.23.8
 )
--- model/user.go
+++ model/user.go
@@ -1,8 +1,55 @@
 package model
 
+import (
+	"errors"
+
+	"golang.org/x/crypto/bcrypt"
+	"gorm.io/gorm"
+)
+
 type User struct {
 	ID       int
 	Nama     string
 	Email    string
 	Password string
+}
+
+type UserModel struct {
+	db *gorm.DB
+}
+
+func (um *UserModel) Insert(newUser User) (User, error) {
+	if err := um.db.Create(&newUser).Error; err != nil {
+		return User{}, err
+	}
+
+	return newUser, nil
+}
+func (um *UserModel) Update(updatedUser User) (User, error) {
+	if err := um.db.Save(&updatedUser).Error; err != nil {
+		return User{}, err
+	}
+
+	return updatedUser, nil
+}
+func (um *UserModel) GetAll() ([]User, error) {
+	var res []User
+	if err := um.db.Find(&res).Error; err != nil {
+		return nil, err
+	}
+	return res, nil
+}
+func (um *UserModel) Login(email string, password string) (User, error) {
+	var res User
+	if err := um.db.Where("email = ?", email).First(&res).Error; err != nil {
+		return User{}, err
+	}
+
+	err := bcrypt.CompareHashAndPassword([]byte(res.Password), []byte(password))
+	if err != nil {
+		return User{}, errors.New("password salah")
+	}
+
+	return res, nil
+
 }
