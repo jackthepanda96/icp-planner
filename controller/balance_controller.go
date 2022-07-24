f9c536a955648b0df8017c2779109921c614fac5
Jerry Young <jerryyoung2911@gmail.com>
2022-07-24 15:41:02 +0700

    Balance operation

--- controller/balance_controller.go
+++ controller/balance_controller.go
@@ -1,1 +1,88 @@
 package controller
+
+import (
+	"net/http"
+
+	"github.com/jackthepanda96/icp-planner/model"
+	"github.com/labstack/echo/v4"
+)
+
+type BalanceController struct {
+	Model model.BalanceModel
+}
+
+func (bc *BalanceController) Income() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		var input model.Balance
+		if err := c.Bind(&input); err != nil {
+			return c.JSON(http.StatusBadRequest, "error when parsing data")
+		}
+		input.Segment = 0
+		email, _ := ExtractInfo(c)
+		input.Email = email
+		res, err := bc.Model.Insert(input)
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, map[string]interface{}{
+				"message": "error from server",
+				"status":  false,
+			})
+		}
+
+		return c.JSON(http.StatusCreated, map[string]interface{}{
+			"message": "success insert income",
+			"status":  true,
+			"data":    []model.Balance{res},
+		})
+	}
+}
+
+func (bc *BalanceController) Expense() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		var input model.Balance
+		if err := c.Bind(&input); err != nil {
+			return c.JSON(http.StatusBadRequest, "error when parsing data")
+		}
+		input.Segment = 1
+		input.Amount *= -1
+		email, _ := ExtractInfo(c)
+		input.Email = email
+		res, err := bc.Model.Insert(input)
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, map[string]interface{}{
+				"message": "error from server",
+				"status":  false,
+			})
+		}
+
+		return c.JSON(http.StatusCreated, map[string]interface{}{
+			"message": "success insert expense",
+			"status":  true,
+			"data":    []model.Balance{res},
+		})
+	}
+}
+
+func (bc *BalanceController) Balance() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		email, _ := ExtractInfo(c)
+		res, total, err := bc.Model.GetBalance(email)
+		if total == -1 {
+			return c.JSON(http.StatusInternalServerError, map[string]interface{}{
+				"message": "error from server",
+				"status":  false,
+			})
+		} else if err != nil {
+			return c.JSON(http.StatusInternalServerError, map[string]interface{}{
+				"message": "error from server",
+				"status":  false,
+			})
+		}
+
+		return c.JSON(http.StatusCreated, map[string]interface{}{
+			"message": "success insert user",
+			"status":  true,
+			"data":    res,
+			"balance": total,
+		})
+	}
+}
--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"log"
 
 	"github.com/jackthepanda96/icp-planner/controller"
 	"github.com/jackthepanda96/icp-planner/model"
@@ -26,14 +25,32 @@ func main() {
 
 	um := model.UserModel{}
 	uc := controller.UserController{Model: um}
+	bm := model.BalanceModel{}
+	bc := controller.BalanceController{Model: bm}
 
 	e.GET("/users", uc.GetAllUSer())
 	e.POST("/users", uc.Register())
 	e.POST("/login", uc.Login())
 	e.PUT("/users/:id", uc.UpdateProfile(), middleware.BasicAuth(func(username, password string, ctx echo.Context) (bool, error) {
-		log.Println(username, password)
-		if res, err := um.Login(username, password); err != nil {
-			log.Println(res, err)
+		if _, err := um.Login(username, password); err != nil {
+			return false, nil
+		}
+		return true, nil
+	}))
+	e.POST("/income", bc.Income(), middleware.BasicAuth(func(username, password string, ctx echo.Context) (bool, error) {
+		if _, err := um.Login(username, password); err != nil {
+			return false, nil
+		}
+		return true, nil
+	}))
+	e.POST("/expense", bc.Expense(), middleware.BasicAuth(func(username, password string, ctx echo.Context) (bool, error) {
+		if _, err := um.Login(username, password); err != nil {
+			return false, nil
+		}
+		return true, nil
+	}))
+	e.GET("/balance", bc.Balance(), middleware.BasicAuth(func(username, password string, ctx echo.Context) (bool, error) {
+		if _, err := um.Login(username, password); err != nil {
 			return false, nil
 		}
 		return true, nil
